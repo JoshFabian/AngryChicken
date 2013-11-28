3178020042c5cad5b8d0a41e911b818029ed2a05
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-11-29 00:29:20 +0530

    Vanilla Rails 4 app without Turbolinks.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngryChicken::Application.config.secret_key_base = '0ab60c24471ccf39c53b89cfef7dc0c736262550e53019ca71aa7b1830c55651c28cb98c0022d4f5aec638687b14f2030c95af56f49954d57912e938b4d68205'
