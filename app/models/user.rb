399f15375215b10599077694c91a7d179272ab74
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-11-29 14:30:05 +0530

    Auth token generation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@
 # Table name: users
 #
 #  id                 :integer          not null, primary key
-#  username           :string(255)
-#  email              :string(255)
-#  encrypted_password :string(255)
-#  auth_token         :string(255)
+#  username           :string(20)       not null
+#  email              :string(255)      not null
+#  encrypted_password :string(255)      not null
+#  auth_token         :string(32)       not null
 #  last_seen_at       :datetime
 #  ip_address         :string(255)
 #  created_at         :datetime
@@ -14,4 +14,12 @@
 #
 
 class User < ActiveRecord::Base
+  before_save :ensure_auth_token_exists
+
+  # Generate auth token if it doesn't exist.
+  def ensure_auth_token_exists
+    if self.auth_token.nil? or self.auth_token.length != 32
+      self.auth_token = SecureRandom.hex(32)
+    end
+  end
 end
--- db/migrate/20131129084648_create_users.rb
+++ db/migrate/20131129084648_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :encrypted_password
-      t.string :auth_token
+      t.string :username, limit: 20, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
+      t.string :auth_token, limit: 32, null: false
       t.datetime :last_seen_at
       t.string :ip_address
 
