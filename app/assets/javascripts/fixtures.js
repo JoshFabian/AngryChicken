cc4f6f65d987734dc7f6b133b754f9760274f952
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-12-01 11:46:42 +0530

    Add basic card Ember model.

--- app/assets/javascripts/fixtures.js
+++ app/assets/javascripts/fixtures.js
@@ -1,3 +1,7 @@
 AngryChicken.User.FIXTURES = [
   {id: 'vikhyat', username: 'vikhyat', email: 'c@vikhyat.net'}
 ];
+
+AngryChicken.Card.FIXTURES = [
+  {id: 'silverback-patriarch', name: 'Silverback Patriarch', attack: 1, defense: 1, manaCost: 3}
+];
---
+++ app/assets/javascripts/models/card.js.coffee
@@ -0,0 +1,6 @@
+AngryChicken.Card = DS.Model.extend
+  name: DS.attr('string')
+  attack: DS.attr('number')
+  defense: DS.attr('number')
+  manaCost: DS.attr('number')
+
